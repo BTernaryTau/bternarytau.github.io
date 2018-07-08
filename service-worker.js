c9fb836d8a799c3ce0ada8e995728069efe78904
BTernaryTau <bternarytau@gmail.com>
2018-07-07 20:02:34 -0400

    Immediately serve new content

--- service-worker.js
+++ service-worker.js
@@ -36,60 +36,27 @@ self.addEventListener('install', function(event) {
 	);
 });
 
-self.addEventListener('activate', function(event) {
-	console.log('Activating new service worker...');
-
-	var cacheWhitelist = [CACHE_NAME];
-
-	event.waitUntil(
-		caches.keys().then(function(cacheNames) {
-			return Promise.all(
-				cacheNames.map(function(cacheName) {
-					if (cacheWhitelist.indexOf(cacheName) === -1) {
-						return caches.delete(cacheName);
-					}
-				})
-			);
-		})
-	);
-});
-
 self.addEventListener('fetch', function(event) {
-	event.respondWith(
-		caches.match(event.request)
-			.then(function(response) {
-				// Cache hit - return response
-				if (response) {
-					return response;
-				}
-
-				// IMPORTANT: Clone the request. A request is a stream and
-				// can only be consumed once. Since we are consuming this
-				// once by cache and once by the browser for fetch, we need
-				// to clone the response.
-				var fetchRequest = event.request.clone();
-
-				return fetch(fetchRequest).then(
-					function(response) {
-						// Check if we received a valid response
-						if(!response || response.status !== 200 || response.type !== 'basic') {
-							return response;
-						}
-
-						// IMPORTANT: Clone the response. A response is a stream
-						// and because we want the browser to consume the response
-						// as well as the cache consuming the response, we need
-						// to clone it so we have two streams.
-						var responseToCache = response.clone();
-
-						caches.open(CACHE_NAME)
-							.then(function(cache) {
-								cache.put(event.request, responseToCache);
-							});
-
-					return response;
-				}
-			);
-		})
-	);
+	console.log('The service worker is serving the asset.');
+	event.respondWith(fromNetwork(event.request, 400).catch(function () {
+		return fromCache(event.request);
+	}));
 });
+
+function fromNetwork(request, timeout) {
+	return new Promise(function(fulfill, reject) {
+		var timeoutId = setTimeout(reject, timeout);
+		fetch(request).then(function(response) {
+			clearTimeout(timeoutId);
+			fulfill(response);
+		}, reject);
+	});
+}
+
+function fromCache(request) {
+	return caches.open(CACHE_NAME).then(function(cache) {
+		return cache.match(request).then(function(matching) {
+			return matching || Promise.reject('no-match');
+		});
+	});
+}
