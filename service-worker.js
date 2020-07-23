521b030b04a734c1da340adfdbeee799874cbbc9
BTernaryTau <bternarytau@gmail.com>
2020-07-23 09:45:15 +0000

    Travis #38

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Update the service worker every time Jekyll processes the site
-const version = '20200722110750';
+const version = '20200723094515';
 
 var CACHE_NAME = 'technically-exists-cache-v' + version;
 var urlsToCache = [
@@ -19,6 +19,8 @@ var urlsToCache = [
 		'/miscellaneous/voting-theory/identical-input-options-criterion',
 	
 		'/about/',
+	
+		'/miscellaneous/simdemocracy/',
 	
 		'/miscellaneous/voting-theory/',
 	
