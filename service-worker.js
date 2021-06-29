fc15b9c91935e3ca1ad4c34ff1c35d48db910c1b
BTernaryTau <bternarytau@gmail.com>
2021-06-29 15:20:39 +0000

    Travis #103

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Update the service worker every time Jekyll processes the site
-const version = '20210629120249';
+const version = '20210629152036';
 
 var CACHE_NAME = 'technically-exists-cache-v' + version;
 var urlsToCache = [
