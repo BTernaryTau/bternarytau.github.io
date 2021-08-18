ba8fc47c2ccaf7f87251a6ed1bf9648d4e214ad1
BTernaryTau <bternarytau@gmail.com>
2021-08-18 23:02:43 +0000

    Travis #116

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Update the service worker every time Jekyll processes the site
-const version = '20210818201435';
+const version = '20210818230236';
 
 var CACHE_NAME = 'technically-exists-cache-v' + version;
 var urlsToCache = [
@@ -39,6 +39,8 @@ var urlsToCache = [
 		'/',
 	
 		'/miscellaneous/voting-theory/list-of-voting-method-simulations',
+	
+		'/miscellaneous/voting-theory/opposite-cancellation-criterion',
 	
 		'/miscellaneous/voting-theory/raws-voting',
 	
