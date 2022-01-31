473794c0d1d24a0627b3b0094bb31517d6bb2d4b
BTernaryTau <bternarytau@gmail.com>
2022-01-31 00:02:01 +0000

    Travis #133

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ gem 'wdm', '>= 0.1.0', :platforms => :x64_mingw
 gem 'jekyll-paginate'
 gem 'jekyll-seo-tag'
 gem 'kramdown-math-katex'
+gem "webrick", "~> 1.7"
--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Update the service worker every time Jekyll processes the site
-const version = '20220130012143';
+const version = '20220131000142';
 
 var CACHE_NAME = 'technically-exists-cache-v' + version;
 var urlsToCache = [
