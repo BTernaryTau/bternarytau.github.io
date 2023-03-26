a8fcaba975f8c1db4582c6827397ee4a22dbcc20
BTernaryTau <bternarytau@gmail.com>
2023-03-26 12:07:57 -0400

    Switch branches for build

--- script/build.sh
+++ script/build.sh
@@ -11,15 +11,15 @@ rm -rf _site
 mkdir _site
 
 # Clone remote repo into the "_site" directory
-git clone https://${GITHUB_ACTOR}:${TOKEN}@github.com/${GITHUB_REPOSITORY}.git --branch gh-pages _site
+git clone https://${GITHUB_ACTOR}:${TOKEN}@github.com/${GITHUB_REPOSITORY}.git --branch master _site
 
 # Build with Jekyll into "_site"
 bundle exec jekyll build
 
-# Push to gh-pages
+# Push to master
 cd _site
 git config user.email "bternarytau@gmail.com"
 git config user.name "BTernaryTau"
 git add --all
 git commit -a -m "Jekyll build for commit ${GITHUB_SHA}"
-git push --force origin gh-pages
+git push --force origin master
