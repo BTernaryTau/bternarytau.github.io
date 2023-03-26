0a46b0522cbf54e43d8575455e2679641afcb13f
BTernaryTau <bternarytau@gmail.com>
2023-03-26 16:23:32 +0000

    Jekyll build for commit d535fb2d84819fab4f8754f889f013f6259f1de6

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Update the service worker every time Jekyll processes the site
-const version = '20230326161402';
+const version = '20230326162317';
 
 var CACHE_NAME = 'technically-exists-cache-v' + version;
 var urlsToCache = [
