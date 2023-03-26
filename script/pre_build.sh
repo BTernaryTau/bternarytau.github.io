624aa1674b0685e65c3637b3981fe32f9a6484ec
BTernaryTau <bternarytau@gmail.com>
2023-03-26 11:04:36 -0400

    Clean up build directory

--- script/pre_build.sh
+++ script/pre_build.sh
@@ -7,6 +7,7 @@ set -e
 pacman -S --noconfirm nodejs npm
 
 # Jekyll Deploy Action uses the build directory as the target for jekyll build
+rm -rf build
 mkdir build
 
 # Clone remote repo into the build directory
