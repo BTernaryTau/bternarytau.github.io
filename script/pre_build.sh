961c82c03239709abbb659d37d8fa20318986733
BTernaryTau <bternarytau@gmail.com>
2023-03-26 02:12:53 -0400

    Update pre_build script

--- script/pre_build.sh
+++ script/pre_build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# Enable error reporting to the console
+set -e
+
 # Install Node.js
 pacman -S --noconfirm nodejs npm
+
+# Jekyll Deploy Action uses the build directory rather than the _site directory
+mkdir build
+
+# Clone remote repo into the build directory
+git clone https://${ACTOR}:${TOKEN}@github.com/${REPOSITORY}.git --branch gh-pages build
