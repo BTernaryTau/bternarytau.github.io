027d03e76310bc5e76bcd3f58a1f707b401c7320
BTernaryTau <bternarytau@gmail.com>
2023-03-26 11:47:57 -0400

    Switch to custom build script

---
+++ script/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Enable error reporting to the console
+set -e
+
+# Install Node.js
+pacman -S --noconfirm nodejs npm
+
+# Install bundler
+gem install bundler -v 2.3.6
+
+# Install gems
+bundle install
+
+# Cleanup "_site"
+rm -rf _site
+mkdir _site
+
+# Clone remote repo into the "_site" directory
+git clone https://${GITHUB_ACTOR}:${TOKEN}@github.com/${GITHUB_REPOSITORY}.git --branch gh-pages _site
+
+# Build with Jekyll into "_site"
+bundle exec jekyll build
+
+# Push to gh-pages
+cd _site
+git config user.email "bternarytau@gmail.com"
+git config user.name "BTernaryTau"
+git add --all
+git commit -a -m "Jekyll build for commit ${GITHUB_SHA}"
+git push --force origin gh-pages
